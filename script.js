7894452c4ed2fc5785924b9ad28a87ffc9fc17c7
Sudeepta Sarmah <sudeepsarmah.gmail.com>
2025-03-09 22:18:56 +0530

    Modified the function to check for palindrome

--- script.js
+++ script.js
@@ -5,25 +5,51 @@ const submitBtn = document.getElementById("check-btn")
 const resultText = document.getElementById("result")
 
 
+// Solution from internet: https://www.freecodecamp.org/news/freecodecamp-palindrome-checker-walkthrough/
+const palindrome = (str) => {
+    const alphaNumericOnly = str
+        // Lowercase the input
+        .toLowerCase()
+        // strip out the non-alphanumeric characters
+        .match(/[a-z0-9]/g)
+
+    // return string === reversedString
+    return alphaNumericOnly.join('') === alphaNumericOnly.reverse().join('')
+}
 
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault()
-    // Using trim() to remove any unwanted white spaces which might give false negatives. Also, getting the trimmed result and storing it in a variable before using it later on
-    const userValue = userInput.value.trim()
-    // Function to return the reversed string
-    const reverseInput = (value) => {
-        return value.split("").reverse().join("")
-    }
-    if (!userValue) {
-        resultText.innerText = "Please input a value"
-    } else if (userValue === reverseInput(userValue)) {
-        resultText.innerText = `${userValue} is a palindrome`
+    const userInputValue = userInput.value.trim()
+    const result = palindrome(userInputValue)
+    if (!userInputValue) {
+        alert("Please input a value")
+    } else if (result) {
+        resultText.innerText = `${userInput.value} is a palindrome`
         userInput.value = ""
     } else {
-        resultText.innerText = `${userValue} is not a palindrome`
+        resultText.innerText = `${userInput.value} is not a palindrome`
         userInput.value = ""
     }
 })
+
+// submitBtn.addEventListener("click", (e) => {
+//     e.preventDefault()
+//     // Using trim() to remove any unwanted white spaces which might give false negatives. Also, getting the trimmed result and storing it in a variable before using it later on
+//     const userValue = userInput.value.trim()
+//     // Function to return the reversed string
+//     const reverseInput = (value) => {
+//         return value.split("").reverse().join("")
+//     }
+//     if (!userValue) {
+//         alert("Please input a value")
+//     } else if (userValue === reverseInput(userValue)) {
+//         resultText.innerText = `${userValue} is a palindrome`
+//         userInput.value = ""
+//     } else {
+//         resultText.innerText = `${userValue} is not a palindrome`
+//         userInput.value = ""
+//     }
+// })
 
 
 // 1st Version
